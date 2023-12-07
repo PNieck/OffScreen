639bd6786a836f7b33a7b375540eb6c7c21a3192
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2023-12-07 17:09:41 +0100

    Add error checkoing

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <EGL/egl.h>
+
 #include <cstddef>
+#include <string>
+#include <sstream>
+#include <stdexcept>
 
   static const EGLint configAttribs[] = {
           EGL_SURFACE_TYPE, EGL_PBUFFER_BIT,
@@ -20,20 +24,35 @@
         EGL_NONE,
   };
 
+
+void assertEGLError(const std::string& msg) {
+	EGLint error = eglGetError();
+
+	if (error != EGL_SUCCESS) {
+		std::stringstream s;
+		s << "EGL error 0x" << std::hex << error << " at " << msg;
+		throw std::runtime_error(s.str());
+	}
+}
+
+
 int main(int argc, char *argv[])
 {
   // 1. Initialize EGL
   EGLDisplay eglDpy = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+  assertEGLError("eglGetDisplay");
 
   EGLint major, minor;
 
   eglInitialize(eglDpy, &major, &minor);
+  assertEGLError("eglInitialize");
 
   // 2. Select an appropriate configuration
   EGLint numConfigs;
   EGLConfig eglCfg;
 
   eglChooseConfig(eglDpy, configAttribs, &eglCfg, 1, &numConfigs);
+  assertEGLError("eglChooseConfig");
 
   // 3. Create a surface
   EGLSurface eglSurf = eglCreatePbufferSurface(eglDpy, eglCfg, 
@@ -41,16 +60,20 @@ int main(int argc, char *argv[])
 
   // 4. Bind the API
   eglBindAPI(EGL_OPENGL_API);
+  assertEGLError("eglBindAPI");
 
   // 5. Create a context and make it current
   EGLContext eglCtx = eglCreateContext(eglDpy, eglCfg, EGL_NO_CONTEXT, 
                                        NULL);
+  assertEGLError("eglCreateContext");
 
   eglMakeCurrent(eglDpy, eglSurf, eglSurf, eglCtx);
+  assertEGLError("eglMakeCurrent");
 
   // from now on use your OpenGL context
 
   // 6. Terminate EGL when finished
   eglTerminate(eglDpy);
+  assertEGLError("eglTerminate");
   return 0;
 }
